2a65d452359b371cd51e9333e238d498f37afaa4
Jim van Vliet <jimvanvliet98@gmail.com>
2016-03-05 18:15:25 +0100

    Testing data again

--- mainScript.js
+++ mainScript.js
@@ -1,6 +1,6 @@
 window.addEventListener("load",function() { // Wait for the window to finish loading
  
-	var Q = window.Q = Quintus()                // Create a new engine instance
+	var Q = window.Q = Quintus({development: true})                // Create a new engine instance
 	    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX") // Load any needed modules
 	    .setup("myGame")                        // Bind Quintus to the canvas with ID "myGame"
 	    .controls()                             // Add in default controls (keyboard, buttons)
@@ -15,7 +15,7 @@ window.addEventListener("load",function() { // Wait for the window to finish loa
 
     Q.loadTMX("Testingground.tmx, sprites.json", function()
     {
-        Q.compileSheets("sprites/Graphics/Autotiles/sprites.png","sprites.json");
+        Q.compileSheets("sprites/Graphics/Autotiles/TestingTile.png","sprites.json");
     	Q.stageScene("Testingground");
     }
     );
