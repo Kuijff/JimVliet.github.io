7a3e0f868f1a82dc02dcd9e3358f521aa5e6c68c
Jim van Vliet <jimvanvliet98@gmail.com>
2016-03-06 10:26:22 +0100

    added test_01 map

--- mainScript.js
+++ mainScript.js
@@ -2,20 +2,20 @@ window.addEventListener("load",function() { // Wait for the window to finish loa
  
 	var Q = window.Q = Quintus({development: true, imagePath: "/sprites/Graphics/Autotiles/"})                // Create a new engine instance
 	    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX") // Load any needed modules
-	    .setup("myGame")                        // Bind Quintus to the canvas with ID "myGame"
+	    .setup("myGame", {scaleToFit: true})                        // Bind Quintus to the canvas with ID "myGame"
 	    .controls()                             // Add in default controls (keyboard, buttons)
 	    .touch();                               // Add in touch support (for the UI)
     
     
-    Q.scene("Testingground", function(stage)
+    Q.scene("test_01", function(stage)
     {
-    	Q.stageTMX("Testingground.tmx", stage);
+    	Q.stageTMX("test_01.tmx", stage);
     }
     );
 
-    Q.loadTMX("Testingground.tmx", function()
+    Q.loadTMX("test_01.tmx", function()
     {
-    	Q.stageScene("Testingground");
+    	Q.stageScene("test_01");
     }
     );
 });
